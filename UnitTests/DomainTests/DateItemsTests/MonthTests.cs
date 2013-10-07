4629e42dd48a2a2c31e2ca7387a299ff0626c4e4
Artyr <rubkaarchi@gmail.com>
2013-10-07 22:37:05 +0300

    minor (UnitTests) : added unit tests for Year and Month

---
+++ UnitTests/DomainTests/DateItemsTests/MonthTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Code.DateItems;
+using Domain.Code.Main;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Common;
+
+namespace UnitTests.DomainTests.DateItemsTests
+{
+    [TestClass]
+    public class MonthTests
+    {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            TestHellper.CreateTagRepository();
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void WrongDateMonth13Test()
+        {
+            var target = new AllTime(new List<CostItemForDataBase>());
+            target.GetYear(2013).GetMonth(1).GetDay(0);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void WrongDateMonth0Test()
+        {
+            var target = new AllTime(new List<CostItemForDataBase>());
+            target.GetYear(2013).GetMonth(1).GetDay(32);
+        }
+    }
+}
---
+++ UnitTests/DomainTests/DateItemsTests/YearTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Code.DateItems;
+using Domain.Code.Main;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Common;
+
+namespace UnitTests.DomainTests.DateItemsTests
+{
+    [TestClass]
+    public class YearTests
+    {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            TestHellper.CreateTagRepository();
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void WrongDateMonth13Test()
+        {
+            var target = new AllTime(new List<CostItemForDataBase>());
+            target.GetYear(2013).GetMonth(13);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [TestMethod]
+        public void WrongDateMonth0Test()
+        {
+            var target = new AllTime(new List<CostItemForDataBase>());
+            target.GetYear(2013).GetMonth(0);
+        }
+    }
+}
