572df23f1035fcfea173b36c4e11d9a267aa5a9c
unknown <artur.korop@kbp1-dhp-f24512.synapse.com>
2013-10-07 17:39:12 +0300

    Major (UnitTests) : added unit test

---
+++ UnitTests/Common/TestHellper.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Code.Common;
+using Domain.Interfaces;
+using Microsoft.Practices.Unity;
+using Moq;
+
+namespace UnitTests.Common
+{
+    public static class TestHellper
+    {
+        public static void CreateTagRepository()
+        {
+            var mockTagRepository = new Mock<ITagRepository>();
+            mockTagRepository.Setup(x => x.GetTag(1)).Returns("Eat");
+            mockTagRepository.Setup(x => x.GetTag(2)).Returns("Sport");
+            mockTagRepository.Setup(x => x.GetTag(3)).Returns("Entertainment");
+            mockTagRepository.Setup(x => x.GetTag(4)).Returns("Novus");
+            mockTagRepository.Setup(x => x.GetTag(It.IsInRange(5, 1000, Range.Exclusive))).Returns("Empty tag");
+
+            DIServiceLocator.Current.RegisterInstance(mockTagRepository.Object);
+        }
+    }
+}
---
+++ UnitTests/DomainTests/DateItemsTests/AllTimeTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Code.DateItems;
+using Domain.Code.Main;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Common;
+
+namespace UnitTests.DomainTests.DateItemsTests
+{
+    [TestClass]
+    public class AllTimeTests
+    {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            TestHellper.CreateTagRepository();
+        }
+
+        [TestMethod]
+        public void SimpleAllTimeTest()
+        {
+            var item = new CostItemForDataBase
+                {
+                    Date = new DateTime(2013, 10, 7),
+                    Id = 1,
+                    Name = "Bread",
+                    Price = 4,
+                    Tags = new List<int> {1, 4, 6},
+                    UserId = 1
+                };
+
+            var target = new AllTime(new List<CostItemForDataBase> {item});
+            var day = target.GetYear(2013).GetMonth(10).GetDay(7);
+            Assert.AreEqual(day.CostItems.Count, 1);
+            var costItem = day.CostItems[0];
+            Assert.AreEqual(costItem.Name, "Bread");
+            Assert.AreEqual(costItem.Price, 4);
+            Assert.AreEqual(costItem.Tags[0], "Eat");
+            Assert.AreEqual(costItem.Tags[1], "Novus");
+            Assert.AreEqual(costItem.Tags[2], "Empty tag");
+        }
+    }
+}
