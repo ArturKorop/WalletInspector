706d67822529e8feec568b92aed7b70607111536
Artur Korop <rubkaarchi@gmail.com>
2013-10-10 14:46:46 +0300

    Major (WebUI) : added Navigation

---
+++ WebUI/Controllers/NavigationController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebUI.Models;
+
+namespace WebUI.Controllers
+{
+    public class NavigationController : Controller
+    {
+        public PartialViewResult Menu(string menuItem = null)
+        {
+            ViewBag.SelectedMenuItem = menuItem;
+            var listMenuItem = new List<MenuLinkModels>
+                {
+                    new MenuLinkModels {Name = "Home", Controller = "Home", Action = "Index"},
+                    new MenuLinkModels {Name = "Wallet", Controller = "Wallet", Action = "Index"},
+                };
+
+            return PartialView(listMenuItem);
+        }
+    }
+}
---
+++ WebUI/Models/MenuLinkModels.cs
@@ -0,0 +1,25 @@
+﻿namespace WebUI.Models
+{
+    /// <summary>
+    /// Class, that provides a description of link
+    /// </summary>
+    public class MenuLinkModels
+    {
+        /// <summary>
+        /// Link name
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Controller, that was call from this link
+        /// </summary>
+        public string Controller { get; set; }
+        /// <summary>
+        /// Action of controllers
+        /// </summary>
+        public string Action { get; set; }
+        /// <summary>
+        /// Some properties
+        /// </summary>
+        public string Properties { get; set; }
+    }
+}
