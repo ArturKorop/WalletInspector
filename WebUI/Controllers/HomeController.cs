a46801635d42d2d330c13b93838448a0a53fd322
Artur Korop <rubkaarchi@gmail.com>
2013-10-10 20:49:05 +0300

    minor (WebUI) : added some code to HomeController

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -3,26 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Domain.Code.Common;
 using Domain.Code.DatabaseItems;
 using Domain.Code.General;
 using Domain.Code.Repository;
 using Domain.Interfaces;
+using Microsoft.Practices.Unity;
 
 namespace WebUI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private readonly IRepository _repository;
+
+        public HomeController()
         {
-            return View();
+            _repository = DIServiceLocator.Current.Resolve<IRepository>();
         }
 
+        public ActionResult Index()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                _repository.Add(new CostItem("Bread" + i, DateTime.Now, 4 + i));
+            }
+
+            return View();
+        }
         
         public ActionResult AddItem(CostItem item)
         {
             item.Date = DateTime.Now;
             item.UserId = 1;
-            var repository = new MsSqlRepository();
+            IRepository repository = new MsSqlRepository();
             repository.Add(item);
 
             return View("Index");
