8a5fdd4384f55be42486352c970264433904d8af
Artur Korop <artyr.korop@gmail.com>
2013-10-11 14:17:27 +0300

    minor (UnitTests) : added unit tests for time classes

--- UnitTests/DomainTests/TimeTests/MonthTests.cs
+++ UnitTests/DomainTests/TimeTests/MonthTests.cs
@@ -10,20 +10,26 @@ namespace UnitTests.DomainTests.TimeTests
     [TestClass]
     public class MonthTests
     {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            TestHellper.CreateTagRepository();
+        }
+
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         [TestMethod]
         public void WrongDateDay0Test()
         {
-            var target = new AllTime(new List<CostItem>());
-            target.GetYear(2013).GetMonth(1).GetDay(0);
+            var target = new Month(2010, 10);
+            target.GetDay(0);
         }
 
         [ExpectedException(typeof(ArgumentOutOfRangeException))]
         [TestMethod]
         public void WrongDateDay32Test()
         {
-            var target = new AllTime(new List<CostItem>());
-            target.GetYear(2013).GetMonth(1).GetDay(32);
+            var target = new Month(2010, 10);
+            target.GetDay(33);
         }
 
         [TestMethod]
@@ -34,6 +40,56 @@ namespace UnitTests.DomainTests.TimeTests
             Assert.AreEqual(target.Days.Length, 31);
             Assert.AreEqual(target.MonthNumber, 10);
             Assert.AreEqual(target.ThisYear, 2010);
+        }
+
+        [TestMethod]
+        public void AddItemTest()
+        {
+            var target = new Month(2010, 10);
+            target.AddCostItem(new CostItem("Bread", new DateTime(2010, 10, 10), 34));
+            var item = target.GetDay(10).CostItems[0];
+
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Name, "Bread");
+            Assert.AreEqual(item.Price,  34);
+            Assert.AreEqual(item.Date, new DateTime(2010, 10, 10));
+
+        }
+
+        [TestMethod]
+        public void AddItemsTest()
+        {
+            var target = new Month(2010, 10);
+            target.AddCostItems(new[] { new CostItem("Bread", new DateTime(2010, 10, 10), 34), new CostItem("Bread2", new DateTime(2010, 10, 11), 11) });
+            var item = target.GetDay(10).CostItems[0];
+
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Name, "Bread");
+            Assert.AreEqual(item.Price, 34);
+            Assert.AreEqual(item.Date, new DateTime(2010, 10, 10));
+
+            item = target.GetDay(11).CostItems[0];
+
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Name, "Bread2");
+            Assert.AreEqual(item.Price, 11);
+            Assert.AreEqual(item.Date, new DateTime(2010, 10, 11));
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void AddWrongItemTest()
+        {
+            var target = new Month(2010, 10);
+            target.AddCostItem(new CostItem("Bread", new DateTime(2010, 11, 10), 34));
+        }
+
+        [ExpectedException(typeof(ArgumentException))]
+        [TestMethod]
+        public void AddWrongItemsTest()
+        {
+            var target = new Month(2010, 10);
+            target.AddCostItems(new[] { new CostItem("Bread", new DateTime(2010, 11, 10), 34), new CostItem("Bread2", new DateTime(2010, 10, 11), 11) });
         }
     }
 }
--- UnitTests/DomainTests/TimeTests/YearTests.cs
+++ UnitTests/DomainTests/TimeTests/YearTests.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using Domain.Code.General;
 using Domain.Code.Time;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Common;
 
 namespace UnitTests.DomainTests.TimeTests
 {
     [TestClass]
     public class YearTests
     {
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            TestHellper.CreateTagRepository();
+        }
+
+        [ExpectedException(typeof (ArgumentOutOfRangeException))]
         [TestMethod]
         public void WrongDateMonth13Test()
         {
@@ -17,7 +24,7 @@ namespace UnitTests.DomainTests.TimeTests
             target.GetYear(2013).GetMonth(13);
         }
 
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [ExpectedException(typeof (ArgumentOutOfRangeException))]
         [TestMethod]
         public void WrongDateMonth0Test()
         {
@@ -25,7 +32,7 @@ namespace UnitTests.DomainTests.TimeTests
             target.GetYear(2013).GetMonth(0);
         }
 
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [ExpectedException(typeof (ArgumentOutOfRangeException))]
         [TestMethod]
         public void WrongYear1800Test()
         {
@@ -34,7 +41,7 @@ namespace UnitTests.DomainTests.TimeTests
             Assert.IsNull(target);
         }
 
-        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [ExpectedException(typeof (ArgumentOutOfRangeException))]
         [TestMethod]
         public void WrongYear2200Test()
         {
@@ -50,6 +57,59 @@ namespace UnitTests.DomainTests.TimeTests
 
             Assert.AreEqual(target.Months.Length, 12);
             Assert.AreEqual(target.Name, 2010);
+        }
+
+        [TestMethod]
+        public void AddItemTest()
+        {
+            var target = new Year(2010);
+            target.AddCostItem(new CostItem("Bread", new DateTime(2010, 10, 10), 34));
+            var item = target.GetMonth(10).GetDay(10).CostItems[0];
+
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Name, "Bread");
+            Assert.AreEqual(item.Price, 34);
+            Assert.AreEqual(item.Date, new DateTime(2010, 10, 10));
+        }
+
+        [TestMethod]
+        public void AddItemsTest()
+        {
+            var target = new Year(2010);
+            target.AddCostItems(new[]
+                {
+                    new CostItem("Bread", new DateTime(2010, 10, 10), 34),
+                    new CostItem("Bread2", new DateTime(2010, 11, 11), 5)
+                });
+            var item = target.GetMonth(10).GetDay(10).CostItems[0];
+
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Name, "Bread");
+            Assert.AreEqual(item.Price, 34);
+            Assert.AreEqual(item.Date, new DateTime(2010, 10, 10));
+
+            item = target.GetMonth(11).GetDay(11).CostItems[0];
+
+            Assert.IsNotNull(item);
+            Assert.AreEqual(item.Name, "Bread2");
+            Assert.AreEqual(item.Price, 5);
+            Assert.AreEqual(item.Date, new DateTime(2010, 11, 11));
+        }
+
+        [ExpectedException(typeof (ArgumentException))]
+        [TestMethod]
+        public void AddWrongItemTest()
+        {
+            var target = new Year(2010);
+            target.AddCostItem(new CostItem("Bread", new DateTime(2011, 10, 10), 34));
+        }
+
+        [ExpectedException(typeof (ArgumentException))]
+        [TestMethod]
+        public void AddWrongItemsTest()
+        {
+            var target = new Year(2010);
+            target.AddCostItems(new[] {new CostItem("Bread", new DateTime(2011, 10, 10), 34)});
         }
     }
 }
