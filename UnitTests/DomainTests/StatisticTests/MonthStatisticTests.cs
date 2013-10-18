4e45c2bd788fa8ed8e223bc26bbbef5c59f4d4bc
Artur Korop <artyr.korop@gmail.com>
2013-10-18 16:34:21 +0300

    minor (UnitTests) : added unit tests for statistic

---
+++ UnitTests/DomainTests/StatisticTests/MonthStatisticTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Domain.Code.General;
+using Domain.Code.Time;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Common;
+
+namespace UnitTests.DomainTests.StatisticTests
+{
+    [TestClass]
+    public class MonthStatisticTests
+    {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            TestHelper.CreateTagRepository();
+        }
+
+        [TestMethod]
+        public void GetMonthTotalSumTest()
+        {
+            var month = new Month(2010,10);
+            month.AddCostItems(new []
+                {
+                    new CostItem("Bread", new DateTime(2010,10,10), 12.56),
+                    new CostItem("Bread", new DateTime(2010,10,11), 256),
+                    new CostItem("Bread", new DateTime(2010,10,12), 0.44),
+                    new CostItem("Bread", new DateTime(2010,10,13), 46),
+                    new CostItem("Bread", new DateTime(2010,10,14), 5)
+                });
+
+            var target = month.Statistic;
+            Assert.AreEqual(target.GetMonthTotalSum(), 320);
+        }
+    }
+}
---
+++ UnitTests/DomainTests/StatisticTests/YearStatisticTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Domain.Code.General;
+using Domain.Code.Time;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTests.Common;
+
+namespace UnitTests.DomainTests.StatisticTests
+{
+    [TestClass]
+    public class YearStatisticTests
+    {
+        [ClassInitialize]
+        public static void ClassInit(TestContext context)
+        {
+            TestHelper.CreateTagRepository();
+        }
+
+        [TestMethod]
+        public void GetMonthTotalSumTest()
+        {
+            var year = new Year(2010);
+            year.AddCostItems(new[]
+                {
+                    new CostItem("Bread", new DateTime(2010,10,10), 12.56),
+                    new CostItem("Bread", new DateTime(2010,10,11), 256),
+                    new CostItem("Bread", new DateTime(2010,10,12), 0.44),
+                    new CostItem("Bread", new DateTime(2010,10,13), 46),
+                    new CostItem("Bread", new DateTime(2010,10,14), 5),
+                    new CostItem("Bread", new DateTime(2010,1,10), 12.56),
+                    new CostItem("Bread", new DateTime(2010,2,11), 256),
+                    new CostItem("Bread", new DateTime(2010,3,12), 0.44),
+                    new CostItem("Bread", new DateTime(2010,4,13), 46),
+                    new CostItem("Bread", new DateTime(2010,7,14), 5)
+                });
+
+            var target = year.Statistic;
+            Assert.AreEqual(target.GetYearTotalSum(), 640);
+        } 
+    }
+}
